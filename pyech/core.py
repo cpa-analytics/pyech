3cdc26c46c297c8e0b439c266ef8ea04221c78cb
Rafael Xavier <rxaviermontero@gmail.com>
2021-10-09 02:22:26 -0300

    More fixes

--- pyech/core.py
+++ pyech/core.py
@@ -210,8 +210,7 @@ class ECH(object):
         dl_dir = Path(dirpath, "dl")
         dl_dir.mkdir(exist_ok=True)
         patoolib.extract_archive(temp_file, outdir=dl_dir, verbosity=-1)
-        survey = [list(Path(dirpath).glob(f"**/*{x}*.sav")) for x in ["h*p", "fusionado"]]
-        survey.extend([list(Path(dirpath).glob(f"**/*{x}*.sav")) for x in ["H*P", "FUSIONADO"]])
+        survey = [list(Path(dirpath).glob(f"**/*{x}*.sav")) for x in ["h*p", "Fusionado", "FUSIONADO"]]
         survey = [x[0] for x in survey if x][0]
         survey.rename(Path(dirpath, f"{year}.sav"))
         shutil.rmtree(dl_dir)
